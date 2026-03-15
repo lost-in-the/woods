5630b9e604db8ac4c1343675befca9784e665acb
Leah Armstrong <code@leah.wtf>
2026-03-14 22:44:10 -0400

    Fix rubocop indentation for inlined_concerns chain

--- lib/woods/extractors/model_extractor.rb
+++ lib/woods/extractors/model_extractor.rb
@@ -328,8 +328,8 @@ module Woods
           scopes: extract_scopes(model, source),
           enums: extract_enums(model),
           inlined_concerns: extract_included_modules(model)
-            .select { |mod| mod.name && concern_source(mod) }
-            .map { |mod| mod.name.demodulize },
+                            .select { |mod| mod.name && concern_source(mod) }
+                            .map { |mod| mod.name.demodulize },
 
           # API surface
           class_methods: model.methods(false).sort,
